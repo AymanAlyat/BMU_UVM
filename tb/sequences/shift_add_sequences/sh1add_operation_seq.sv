f717a2fa3034a4e44e82fa51d73fa79f8b64666b
Ayman Alyat <aymanaleliat@gmail.com>
2026-09-09 11:25:43 +0300

    Add shift_Add Zba sequences

---
+++ tb/sequences/shift_add_sequences/sh1add_operation_seq.sv
@@ -0,0 +1,46 @@
+class sh1add_operation_seq extends uvm_sequence #(bmu_sequence_item);
+
+  `uvm_object_utils(sh1add_operation_seq)
+
+  bmu_sequence_item req;
+
+
+  function new(string name = "sh1add_operation_seq");
+
+    super.new(name);
+  endfunction
+
+
+  task body();
+
+
+    req = bmu_sequence_item::type_id::create("req");
+
+    repeat (20) begin
+
+      start_item(req);
+
+      if (!req.randomize())
+      
+        `uvm_fatal("RAND_ERROR", "Randomization failed")
+
+      
+      req.ap             = '0;
+      req.rst_l          = 1'b1;
+      req.scan_mode      = 1'b0;
+      req.valid_in       = 1'b1;
+      req.csr_ren_in     = 1'b0;
+      req.csr_rddata_in  = 32'b0;
+
+      // SH1ADD operation
+      //Override on req.ap=0
+      req.ap.sh1add = 1'b1;
+      req.ap.zba    = 1'b1;
+
+      finish_item(req);
+
+    end
+
+  endtask
+
+endclass
